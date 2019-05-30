942126a244b8f2b2abc9a10c3fdfda9ac6ced72e
nastyaNSK <nastavlasenkova@gmail.com>
2019-05-30 22:07:42 +0700

    Interface for load and save

--- SystAnalys_lr1/Form1.Designer.cs
+++ SystAnalys_lr1/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.drawVertexButton = new System.Windows.Forms.Button();
             this.sheet = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.sheet)).BeginInit();
             this.SuspendLayout();
             // 
@@ -117,11 +118,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Save_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(710, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "LOAD";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(969, 444);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.selectButton);
             this.Controls.Add(this.deleteALLButton);
@@ -149,6 +160,7 @@
         private System.Windows.Forms.Button selectButton;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
